ef951f182b0380358c3672399b223e244f937103
saubhagyasharma99 <92671977+saubhagyasharma99@users.noreply.github.com>
2022-05-11 14:53:03 +0100

    updated condition in line 98

--- LMS/LibraryMgmtSystem/src/main/java/com/lms/app/service/BorrowServiceImpl.java
+++ LMS/LibraryMgmtSystem/src/main/java/com/lms/app/service/BorrowServiceImpl.java
@@ -1,68 +1,115 @@
 package com.lms.app.service;
 
-
-
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.lms.app.entity.Borrow;
-import com.lms.app.exception.config.BorrowNotFoundException;
-import com.lms.app.repository.BorrowRepository;
+import com.lms.app.entity.Author;
+import com.lms.app.entity.Book;
+import com.lms.app.exception.config.BookNotFoundException;
+import com.lms.app.repository.AuthorRepository;
+import com.lms.app.repository.BookRepository;
 
 @Service
 @Transactional
-public class BorrowServiceImpl implements BorrowService{
+public class BookServiceImpl implements BookService{
+
+	private BookRepository bookRepository;
+	private AuthorRepository authorRepository;
 	
 	@Autowired
-	BorrowRepository borrowRepository;
-
-	@Autowired
-	BookService bookService;
-	
-	@Override
-	public List<Borrow> getAllBorrows() {
-		
-		return borrowRepository.findAll();
-	}
-
-	@Override
-	public Borrow getBorrowById(long id) {
-		
-		return borrowRepository.findById(id)
-				.orElseThrow(() -> new BorrowNotFoundException(id));
+	public BookServiceImpl(BookRepository bookRepository, AuthorRepository authorRepository) {
+		this.bookRepository = bookRepository;
+		this.authorRepository = authorRepository;
 	
 	}
-
 	@Override
-	public Borrow addNewBorrow(Borrow borrow) {
+	public Book saveBook(Book newBook) {
 		
-		return borrowRepository.save(borrow);
+		return bookRepository.save(newBook);
 	}
 
 	@Override
-	public Borrow updateBorrow(Borrow borrow, long id) {
+	public Book getBookById(Long id) {
 		
-		return borrowRepository.findById(id)
-				.map(b -> {
-			    	b.setUsername(borrow.getUsername());
-			        b.setDatetime(borrow.getDatetime());
-			        b.setBook(bookService.getBookById(borrow.getBook().getId()));
+		return bookRepository.findById(id)
+				.orElseThrow(() -> new BookNotFoundException(id));
+	}
+
+	
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return bookRepository.findAll();
+	}
+
+	@Override
+	public Book updateBook(Book newBook, Long id) {
+		
+		return bookRepository.findById(id)
+				.map(book -> {
+			    	book.setIsbn(newBook.getIsbn());
+			        book.setTitle(newBook.getTitle());
+			        book.setStatus(newBook.getStatus());
+			        book.setAuthors(newBook.getAuthors());
 			        
-			        return borrowRepository.save(b);
+			        return bookRepository.save(book);
 			      })
 			      .orElseGet(() -> {
-			        borrow.setId(id);
-			        return borrowRepository.save(borrow);
+			        newBook.setId(id);
+			        return bookRepository.save(newBook);
 			      });
 	}
-
+	
 	@Override
-	public void deleteBorrow(long id) {
+	public void deleteBookById(Long id) {
 		
-		 borrowRepository.deleteById(id);
+		bookRepository.deleteById(id);
+		
 	}
+	@Override
+	public List<Book> getBooksByIsbn(String isbn) {
+		
+		return bookRepository.findByIsbn(isbn)
+				.orElseThrow(()-> new BookNotFoundException(isbn));
+	}
+	@Override
+	public List<Book> getBooksByTitle(String title) {
+		
+		return bookRepository.findByTitle(title)
+				.orElseThrow(()-> new BookNotFoundException(title));
+	}
+	@Override
+	public List<Book> getBooksByAuthor(Long id) {
+		Author author = authorRepository.findById(id)
+				.orElse(new Author());
+		return bookRepository.findByAuthorsIn(Set.of(author))
+				.orElseThrow(()-> new BookNotFoundException(author.toString()));
+	}
+	
+	@Override
+	public Book updateBookStatus(Book newBook, Long id) {
+		 //Update book status (from AVAILABLE or BORROWED and vice versa)
+		return bookRepository.findById(id)
+				.map(book -> {
+					if(book.getStatus()!=null && book.getStatus().equals("AVAILABLE")) {
+			        book.setStatus("BORROWED");
+					}
+					else {
+						book.setStatus("AVAILABLE");
+					}
+			        
+			        return bookRepository.save(book);
+			      })
+			      .orElseGet(() -> {
+			        newBook.setId(id);
+			        newBook.setStatus("AVAILABLE"); 
+			        return bookRepository.save(newBook);
+			      });
+	}
+	
 
 }
